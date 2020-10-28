2e1e6f81e6852b51cf92a3b3c8395f367fff9b7b
1810050 <67385245+1810050@users.noreply.github.com>
2020-10-28 19:13:41 +0600

    readme updated

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,11 +1,20 @@
+// const firebaseConfig = {
+//     apiKey: "AIzaSyATdjHJC7r797plk-D8bKxSCi-YWskWTeM",
+//     authDomain: "hotel-app-a6307.firebaseapp.com",
+//     databaseURL: "https://hotel-app-a6307.firebaseio.com",
+//     projectId: "hotel-app-a6307",
+//     storageBucket: "hotel-app-a6307.appspot.com",
+//     messagingSenderId: "257289457682",
+//     appId: "1:257289457682:web:9ff03c6ef72d032b0d48cb"
+//   };
 const firebaseConfig = {
-    apiKey: "AIzaSyATdjHJC7r797plk-D8bKxSCi-YWskWTeM",
-    authDomain: "hotel-app-a6307.firebaseapp.com",
-    databaseURL: "https://hotel-app-a6307.firebaseio.com",
-    projectId: "hotel-app-a6307",
-    storageBucket: "hotel-app-a6307.appspot.com",
-    messagingSenderId: "257289457682",
-    appId: "1:257289457682:web:9ff03c6ef72d032b0d48cb"
-  };
+  apiKey: "AIzaSyCdyCMl9pWuWTMAf2S9VKRoPpLxf1en3rU",
+  authDomain: "volunteernetwork-ad5f9.firebaseapp.com",
+  databaseURL: "https://volunteernetwork-ad5f9.firebaseio.com",
+  projectId: "volunteernetwork-ad5f9",
+  storageBucket: "volunteernetwork-ad5f9.appspot.com",
+  messagingSenderId: "724988188138",
+  appId: "1:724988188138:web:926143223afa9d8bb08e93"
+};
 
   export default firebaseConfig;
